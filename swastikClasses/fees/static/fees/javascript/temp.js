5bbaa2f3fe5ca0042809e24861d2c14afeb0dd5d
JSSaini07 <mr.jssaini07@gmail.com>
2016-11-03 17:52:00 +0530

    render logic change

--- swastikClasses/fees/static/fees/javascript/temp.js
+++ swastikClasses/fees/static/fees/javascript/temp.js
@@ -355,7 +355,7 @@ function renderPartial(fileName,data){
 		for(var j=0;j<values.length;j++)
 		{
 			$('.'+currentRowKey+'x'+values[j]).text(data[currentRowKey][values[j]].val);
-			if(data[currentRowKey][values[j]].val){
+			if(data[currentRowKey][values[j]].status){
 				$('.'+currentRowKey+'x'+values[j]).addClass('locked');	
 			}
 		}
